e8ee29d97bb4107902283411880db1471a73d172
TheComputerM <mudit.harshit99@outlook.com>
2020-07-03 11:20:16 +0530

    Updated deploys and switched to netlify

--- deploy.js
+++ deploy.js
@@ -1,10 +1,12 @@
 const ncp = require("ncp").ncp;
+const del =require("del");
 const simpleGit = require("simple-git");
 const git = simpleGit();
 
 git
   .init()
   .then(() => git.checkout("docs"))
+  .then(() => del(["*", "!.gitignore", "!dist", "!node_modules", "!site"]))
   .then(() =>
     ncp("./site/__sapper__/export/", "./", (err) => {
       if (err) return console.error(err);
@@ -15,4 +17,5 @@ git
   .then(() => git.commit(`Deployed at ${new Date().toUTCString()}`))
   .then(() => git.push())
   .then(() => git.checkout("master"))
+  .then(() => console.log("Successful"))
   .catch((e) => console.error(e));
