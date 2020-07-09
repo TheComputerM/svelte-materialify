caeabbcdcf6aaf1c7275e4dd79da3a630191b12e
TheComputerM <mudit.harshit99@outlook.com>
2020-07-09 22:13:37 +0530

    Updated packages

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,11 @@
-const sveltePreprocess = require('svelte-preprocess');
+const sveltePreprocess = require("svelte-preprocess");
 module.exports = {
   preprocess: sveltePreprocess({
     scss: {
-      includePaths: ['src'],
+      includePaths: ["src", "theme"],
     },
     postcss: {
-      plugins: [require('autoprefixer')],
+      plugins: [require("autoprefixer")],
     },
   }),
 };
