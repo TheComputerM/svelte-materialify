f8fcd17da204cd4667242843d34900aa7d5ad7a4
TheComputerM <mudit.harshit99@outlook.com>
2020-07-10 17:54:53 +0530

    Added Alerts

--- site/src/routes/index.svelte
+++ site/src/routes/index.svelte
@@ -1,14 +1,34 @@
 <script>
-import {Button} from "svelte-materialify/src/"
+  import { Button, Icon, Alert } from "svelte-materialify/src/";
 </script>
 
 <svelte:head>
-	<title>Svelte Materialify</title>
+  <title>Svelte Materialify</title>
 </svelte:head>
 
 <h1>Great success!</h1>
 <Button>Button</Button>
 <Button text>Button</Button>
 <Button outlined>Button</Button>
+<Button icon><Icon>cloud</Icon></Button>
+<br><br>
+<Alert class="info-color white-text" border="left" icon="check" dismissible>
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, rerum
+  eveniet. Commodi, perferendis. Molestiae itaque quia accusantium optio iure,
+  quod temporibus vel deleniti, nesciunt nemo dolor, illum perspiciatis adipisci
+  exercitationem.
+</Alert>
 
+<Alert class="error-text" coloredBorder text border="bottom" icon="error">
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, rerum
+  eveniet. Commodi, perferendis. Molestiae itaque quia accusantium optio iure,
+  quod temporibus vel deleniti, nesciunt nemo dolor, illum perspiciatis adipisci
+  exercitationem.
+</Alert>
 
+<Alert class="success-text" dense outlined tile border="right" dismissible>
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, rerum
+  eveniet. Commodi, perferendis. Molestiae itaque quia accusantium optio iure,
+  quod temporibus vel deleniti, nesciunt nemo dolor, illum perspiciatis adipisci
+  exercitationem.
+</Alert>
---
+++ src/components/Alert/Alert.svelte
@@ -0,0 +1,56 @@
+<script>
+  import ripplet from "../../actions/Ripple/ripplet";
+
+  import Button from "../Button/Button.svelte";
+  import Icon from "../Icon/Icon.svelte";
+
+  let classes = "";
+  export let visible = true;
+  export let icon = false;
+  export let closeIcon = "cancel";
+  export let dense = false;
+  export let outlined = false;
+  export let text = false;
+  export let tile = false;
+  export let dismissible = false;
+  export let border = false;
+  export let coloredBorder = false;
+  export { classes as class };
+
+  export function dismiss() {
+    visible = false;
+  }
+</script>
+
+<style lang="scss" src="./index.scss">
+
+</style>
+
+{#if visible}
+  <div
+    role="alert"
+    class="s-alert {classes}"
+    class:dense
+    class:outlined
+    class:text
+    class:tile>
+    <div class="wrapper">
+      {#if icon}
+        <Icon class="mr-4 test">{icon}</Icon>
+      {/if}
+      <div class="content">
+        <slot />
+      </div>
+      {#if dismissible}
+        <Button icon ripple={{ appendTo: 'parent' }} on:click={dismiss}>
+          <Icon>{closeIcon}</Icon>
+        </Button>
+      {/if}
+      {#if border}
+        <div
+          class="alert-border border-{border}"
+          class:colored={coloredBorder} />
+      {/if}
+    </div>
+  </div>
+{/if}
