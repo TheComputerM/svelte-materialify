a26808c6072553bccdbb47502900af91a411e399
TheComputerM <mudit.harshit99@outlook.com>
2020-07-30 20:40:11 +0530

    Added Subheader

---
+++ src/components/Subheader/Subheader.svelte
@@ -0,0 +1,12 @@
+<script>
+  let classes = "";
+  export let inset = false;
+  export let style = undefined;
+  export {classes as class};
+</script>
+
+<style lang="scss" src="./index.scss"></style>
+
+<div class="s-subheader {classes}" class:inset {style}>
+  <slot></slot>
+</div>
