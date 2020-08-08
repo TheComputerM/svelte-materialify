1e78f754edc4b4e75421fe707c7998a6048d118d
TheComputerM <mudit.harshit99@outlook.com>
2020-08-08 23:39:46 +0530

    feat(Icon): added support for svg

--- src/components/Icon/Icon.svelte
+++ src/components/Icon/Icon.svelte
@@ -2,7 +2,9 @@
   let classes = '';
   export let inactive = false;
   export let size = '24px';
-  export let md = false;
+  export let path = null;
+  export let title = null;
+  export let rotate = '0deg';
   export let style = '';
   export { classes as class };
 </script>
@@ -13,9 +15,22 @@
 
 <i
   aria-hidden="true"
-  style="font-size:{size};{style}"
+  style="font-size: {size};transform: rotate({rotate});{style}"
   class="s-icon {classes}"
-  class:material-icons={md}
+  {title}
   class:inactive>
+  {#if path}
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      viewBox="0 0 24 24">
+      <path d={path}>
+        {#if title}
+          <title>{title}</title>
+        {/if}
+      </path>
+    </svg>
+  {/if}
   <slot />
 </i>
