dfb9422019abc7e171303416b8add13707c488c3
TheComputerM <mudit.harshit99@outlook.com>
2020-08-19 19:56:20 +0530

    docs: updated example

--- packages/docs/src/examples/components/alerts/dismissible.svelte
+++ packages/docs/src/examples/components/alerts/dismissible.svelte
@@ -1,17 +1,35 @@
 <script>
-  import Alert from 'svelte-materialify/src/components/Alert';
-  import Icon from 'svelte-materialify/src/components/Icon';
-  import Button from 'svelte-materialify/src/components/Button';
+  import Alert from "svelte-materialify/src/components/Alert";
+  import Icon from "svelte-materialify/src/components/Icon";
+  import Button from "svelte-materialify/src/components/Button";
 
+  let timesDismissed = 0;
   let alert = true;
+
+  function reset() {
+    alert = true;
+  }
+
+  function onDismiss() {
+    timesDismissed += 1;
+  }
 </script>
 
 {#if alert}
-  <Alert class="primary-color" dismissible bind:visible={alert}>
+  <Alert
+    class="primary-color"
+    dismissible
+    bind:visible={alert}
+    on:dismiss={onDismiss}>
     Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem molestiae
     voluptates et quae rem autem quasi officia odit suscipit dolor.
   </Alert>
-  <Alert class="primary-color" dismissible bind:visible={alert}>
+
+  <Alert
+    class="primary-color"
+    dismissible
+    bind:visible={alert}
+    on:dismiss={onDismiss}>
     <div slot="close">
       <Icon class="mdi mdi-alert-circle" />
     </div>
@@ -19,5 +37,9 @@
     voluptates et quae rem autem quasi officia odit suscipit dolor.
   </Alert>
 {:else}
-  <Button class="primary-color" on:click={() => (alert = true)}>Reset</Button>
+  <div class='text-center'>
+    <Button class="primary-color" on:click={reset}>Reset</Button>
+    <br>
+    <span>Times Dismissed: {timesDismissed}</span>
+  </div>
 {/if}
