63bfdd9b9542c701262919aefd5e73f81fd60f72
TheComputerM <mudit.harshit99@outlook.com>
2020-08-20 01:40:22 +0530

    fix(Alert): tile styles

--- packages/svelte-materialify/src/components/Avatar/Avatar.svelte
+++ packages/svelte-materialify/src/components/Avatar/Avatar.svelte
@@ -1,12 +1,25 @@
 <script>
-  let classes = '';
-  export let size = '48px';
-  export let tile = false;
+  // Classes to add to avatar.
+  let classes = "";
   export { classes as class };
+
+  // Height and width of the avatar.
+  export let size = "48px";
+
+  // Removes border radius
+  export let tile = false;
+
+  // Styles for avatar
+  export let style = "";
 </script>
 
-<style lang="scss" src="./index.scss"></style>
+<style lang="scss" src="./index.scss">
 
-<div style="height:{size};width:{size}" class="s-avatar {classes}" class:tile>
-  <slot></slot>
+</style>
+
+<div
+  style="height:{size};width:{size};{style}"
+  class="s-avatar {classes}"
+  class:tile>
+  <slot />
 </div>
