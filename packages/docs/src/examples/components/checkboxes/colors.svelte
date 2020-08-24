a499dec6d26a4d555271b05b552f7a59b7b6d16b
TheComputerM <mudit.harshit99@outlook.com>
2020-08-24 22:55:08 +0530

    fix(Checkbox): colors and disabled

--- packages/docs/src/examples/components/checkboxes/colors.svelte
+++ packages/docs/src/examples/components/checkboxes/colors.svelte
@@ -4,9 +4,9 @@
 
 <Checkbox activeClass="red-text">Red when active.</Checkbox>
 <Checkbox class="blue-text" activeClass="blue-text">Always blue.</Checkbox>
-<Checkbox class="green-text" activeClass="green-text">
-  <span class="text--primary">Always green, but not the text.</span>
-</Checkbox>
-<Checkbox class="cyan-text" activeClass="indigo-text">
-  Cyan to indigo.
-</Checkbox>
+<br />
+<div class="d-flex justify-space-between">
+  <Checkbox checked activeClass="secondary-text">secondary</Checkbox>
+  <Checkbox checked activeClass="error-text">error</Checkbox>
+  <Checkbox checked activeClass="success-text">success</Checkbox>
+</div>
--- packages/svelte-materialify/src/components/Checkbox/Checkbox.svelte
+++ packages/svelte-materialify/src/components/Checkbox/Checkbox.svelte
@@ -5,7 +5,7 @@
   };
 
   const check = 'M21,7L9,19L3.5,13.5L4.91,12.09L9,16.17L19.59,5.59L21,7Z';
-  const dash = 'M4,10.5L4,13.5L20,13.5L20,10.5L4,10.5Z';
+  const dash = 'M4,11L4,13L20,13L20,11L4,11Z';
 </script>
 
 <script>
@@ -63,9 +63,10 @@
 
 </style>
 
-<div class="s-checkbox {classes}" {style}>
+<div class="s-checkbox" {style}>
   <div
-    class="s-checkbox__wrapper {checked || indeterminate ? activeClass : ''}"
+    class="s-checkbox__wrapper {classes} {checked || indeterminate ? activeClass : ''}"
+    class:disabled
     use:Ripple={{ centered: true }}>
     <input
       type="checkbox"
@@ -77,7 +78,7 @@
       {value}
       on:change={groupUpdate}
       on:change />
-    <div class="s-checkbox__background">
+    <div class="s-checkbox__background" aria-hidden="true">
       {#if checked || indeterminate}
         <svg
           xmlns="http://www.w3.org/2000/svg"
