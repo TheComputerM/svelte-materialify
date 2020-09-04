98cec18d4b2b8e49ef20e2854e2a8aa0409bea5a
TheComputerM <mudit.harshit99@outlook.com>
2020-09-04 21:17:15 +0530

    ci: better commits

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,4 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: { 'scope-case': [0, 'always', 'lower-case'] },
+};
