1f581c011c4b865d8d60c81cb769006f28d2d5eb
TheComputerM <mudit.harshit99@outlook.com>
2020-09-10 17:29:48 +0530

    feat: added better material ripple

--- packages/svelte-materialify/src/actions/Ripple/index.js
+++ packages/svelte-materialify/src/actions/Ripple/index.js
@@ -1,8 +1,50 @@
-import { RippleBind } from 'material-ripple-effect';
+import { RippleStart, RippleStop } from './material-ripple';
 
-export default (node, options) => {
-  if (typeof options === 'boolean' && !options) {
-    return null;
+export default (node, options = {}) => {
+  if (typeof options !== 'boolean' || options) {
+    let ripple;
+    let keyboardActive = false;
+    const handleStart = (e) => {
+      ripple = RippleStart(e, options);
+    };
+    const handleStop = () => RippleStop(ripple);
+    const handleRemove = () => ripple.remove();
+    const handleKeyboardStart = (e) => {
+      if (!keyboardActive && (e.keyCode === 13 || e.keyCode === 32)) {
+        ripple = RippleStart(e, { ...options, centered: true });
+        keyboardActive = true;
+      }
+    };
+    const handleKeyboardStop = () => {
+      keyboardActive = false;
+      handleStop();
+    };
+
+    node.classList.add('s-ripple-container');
+    node.addEventListener('touchstart', handleStart, { passive: true });
+    node.addEventListener('touchmove', handleRemove, { passive: true });
+    node.addEventListener('touchcancel', handleRemove, false);
+    node.addEventListener('touchend', handleStop, { passive: true });
+    node.addEventListener('mousedown', handleStart);
+    node.addEventListener('mouseup', handleStop);
+    node.addEventListener('mouseleave', handleStop);
+    node.addEventListener('keydown', handleKeyboardStart);
+    node.addEventListener('keyup', handleKeyboardStop);
+
+    return {
+      destroy() {
+        node.classList.remove('s-ripple-container');
+        node.removeEventListener('touchstart', handleStart);
+        node.removeEventListener('touchmove', handleRemove);
+        node.removeEventListener('touchcancel', handleRemove);
+        node.removeEventListener('touchend', handleStop);
+        node.removeEventListener('mousedown', handleStart);
+        node.removeEventListener('mouseup', handleStop);
+        node.removeEventListener('mouseleave', handleStop);
+        node.removeEventListener('keydown', handleKeyboardStart);
+        node.removeEventListener('keyup', handleKeyboardStop);
+      },
+    };
   }
-  return RippleBind(node, options);
+  return null;
 };
---
+++ packages/svelte-materialify/src/actions/Ripple/material-ripple.js
@@ -0,0 +1,84 @@
+/* eslint-disable no-param-reassign */
+
+/**
+ * Options for customizing ripples
+ */
+const defaults = {
+  color: 'currentColor',
+  class: '',
+  opacity: 0.1,
+  centered: false,
+  spreadingDuration: '.4s',
+  spreadingDelay: '0s',
+  spreadingTimingFunction: 'linear',
+  clearingDuration: '1s',
+  clearingDelay: '0s',
+  clearingTimingFunction: 'ease-in-out',
+};
+
+/**
+ * Creates a ripple element but does not destroy it (use RippleStop for that)
+ *
+ * @param {Event} e
+ * @param {*} options
+ * @returns Ripple element
+ */
+export function RippleStart(e, options = {}) {
+  e.stopImmediatePropagation();
+  const opts = { ...defaults, ...options };
+
+  // Parent element
+  const target = e.touches ? e.touches[0].currentTarget : e.currentTarget;
+
+  // Create ripple
+  const ripple = document.createElement('div');
+  const rippleStyle = ripple.style;
+
+  // Adding default stuff
+  ripple.className = `material-ripple ${opts.class}`;
+  rippleStyle.position = 'absolute';
+  rippleStyle.color = 'inherit';
+  rippleStyle.borderRadius = '50%';
+  rippleStyle.pointerEvents = 'none';
+  rippleStyle.width = '100px';
+  rippleStyle.height = '100px';
+  rippleStyle.marginTop = '-50px';
+  rippleStyle.marginLeft = '-50px';
+  target.appendChild(ripple);
+  rippleStyle.opacity = opts.opacity;
+  rippleStyle.transition = `transform ${opts.spreadingDuration} ${opts.spreadingTimingFunction} ${opts.spreadingDelay},opacity ${opts.clearingDuration} ${opts.clearingTimingFunction} ${opts.clearingDelay}`;
+  rippleStyle.transform = 'scale(0) translate(0,0)';
+  rippleStyle.background = opts.color;
+
+  // Positioning ripple
+  const targetRect = target.getBoundingClientRect();
+  if (opts.centered) {
+    rippleStyle.top = `${targetRect.height / 2}px`;
+    rippleStyle.left = `${targetRect.width / 2}px`;
+  } else {
+    const distY = e.clientY || e.touches[0].clientY;
+    const distX = e.clientX || e.touches[0].clientX;
+    rippleStyle.top = `${distY - targetRect.top}px`;
+    rippleStyle.left = `${distX - targetRect.left}px`;
+  }
+
+  // Enlarge ripple
+  rippleStyle.transform = `scale(${
+    Math.max(targetRect.width, targetRect.height) * 0.02
+  }) translate(0,0)`;
+  return ripple;
+}
+
+/**
+ * Destroys the ripple, slowly fading it out.
+ *
+ * @param {Element} ripple
+ */
+export function RippleStop(ripple) {
+  if (ripple) {
+    ripple.addEventListener('transitionend', (e) => {
+      if (e.propertyName === 'opacity') ripple.remove();
+    });
+    ripple.style.opacity = 0;
+  }
+}
