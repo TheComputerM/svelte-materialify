e4d0eff8b89a7fa574e97d0201e880ce2566ad05
TheComputerM <mudit.harshit99@outlook.com>
2020-09-12 21:03:50 +0530

    test: added tests for internal actions

--- packages/svelte-materialify/tests/internal/spec.js
+++ packages/svelte-materialify/tests/internal/spec.js
@@ -1,4 +1,7 @@
 import Class from '@s/internal/Class';
+import Style from '@s/internal/Style';
+import TextColor from '@s/internal/TextColor';
+import BackgroundColor from '@s/internal/BackgroundColor';
 
 describe('internal actions', () => {
   test('Class', () => {
@@ -22,5 +25,60 @@ describe('internal actions', () => {
     update([false && 'a', false && 'b']);
     expect(classList.contains('a')).toBe(false);
     expect(classList.contains('b')).toBe(false);
+  });
+
+  test('Style', () => {
+    document.body.appendChild(document.createElement('div'));
+
+    const div = document.querySelector('div');
+    const { style } = div;
+
+    const { update } = Style(div, { test: '10px' });
+
+    expect(style.getPropertyValue('--s-test')).toBe('10px');
+
+    update({ test2: '20px' });
+    expect(style.getPropertyValue('--s-test')).toBeFalsy();
+    expect(style.getPropertyValue('--s-test2')).toBe('20px');
+  });
+
+  test('TextColor', () => {
+    document.body.appendChild(document.createElement('div'));
+
+    const div = document.querySelector('div');
+    const { classList, style } = div;
+
+    const { update } = TextColor(div, 'primary');
+
+    expect(classList.contains('primary-text')).toBe(true);
+
+    update('red accent-4');
+    expect(classList.contains('primary-text')).toBe(false);
+    expect(classList.contains('red-text')).toBe(true);
+    expect(classList.contains('text-accent-4')).toBe(true);
+
+    update('#ff0000');
+    expect(classList.contains('red-text')).toBe(false);
+    expect(style.color).toBeTruthy();
+  });
+
+  test('BackgroundColor', () => {
+    document.body.appendChild(document.createElement('div'));
+
+    const div = document.querySelector('div');
+    const { classList, style } = div;
+
+    const { update } = BackgroundColor(div, 'primary');
+
+    expect(classList.contains('primary-color')).toBe(true);
+
+    update('red accent-4');
+    expect(classList.contains('primary-color')).toBe(false);
+    expect(classList.contains('red')).toBe(true);
+    expect(classList.contains('accent-4')).toBe(true);
+
+    update('#ff0000');
+    expect(classList.contains('red')).toBe(false);
+    expect(style.backgroundColor).toBeTruthy();
   });
 });
