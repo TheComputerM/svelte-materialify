83ffe9b2de8f8315dab57f73be2cb6a890ddb535
TheComputerM <mudit.harshit99@outlook.com>
2020-09-20 00:01:39 +0530

    feat: reactive ripple

--- packages/svelte-materialify/src/actions/Ripple/index.js
+++ packages/svelte-materialify/src/actions/Ripple/index.js
@@ -3,26 +3,27 @@ import { RippleStart, RippleStop } from './material-ripple';
 /**
  * @param node {Element}
  */
-export default (node, options = {}) => {
-  if (options) {
-    let ripple;
-    let keyboardActive = false;
-    const handleStart = (e) => {
-      ripple = RippleStart(e, options);
-    };
-    const handleStop = () => RippleStop(ripple);
-    const handleRemove = () => ripple.remove();
-    const handleKeyboardStart = (e) => {
-      if (!keyboardActive && (e.keyCode === 13 || e.keyCode === 32)) {
-        ripple = RippleStart(e, { ...options, centered: true });
-        keyboardActive = true;
-      }
-    };
-    const handleKeyboardStop = () => {
-      keyboardActive = false;
-      handleStop();
-    };
+export default (node, _options = {}) => {
+  let options = _options;
+  let ripple;
+  let keyboardActive = false;
+  const handleStart = (e) => {
+    ripple = RippleStart(e, options);
+  };
+  const handleStop = () => RippleStop(ripple);
+  const handleRemove = () => ripple.remove();
+  const handleKeyboardStart = (e) => {
+    if (!keyboardActive && (e.keyCode === 13 || e.keyCode === 32)) {
+      ripple = RippleStart(e, { ...options, centered: true });
+      keyboardActive = true;
+    }
+  };
+  const handleKeyboardStop = () => {
+    keyboardActive = false;
+    handleStop();
+  };
 
+  function setup() {
     node.classList.add('s-ripple-container');
     node.addEventListener('touchstart', handleStart, { passive: true });
     node.addEventListener('touchmove', handleRemove, { passive: true });
@@ -33,21 +34,29 @@ export default (node, options = {}) => {
     node.addEventListener('mouseleave', handleStop);
     node.addEventListener('keydown', handleKeyboardStart);
     node.addEventListener('keyup', handleKeyboardStop);
-
-    return {
-      destroy() {
-        node.classList.remove('s-ripple-container');
-        node.removeEventListener('touchstart', handleStart);
-        node.removeEventListener('touchmove', handleRemove);
-        node.removeEventListener('touchcancel', handleRemove);
-        node.removeEventListener('touchend', handleStop);
-        node.removeEventListener('mousedown', handleStart);
-        node.removeEventListener('mouseup', handleStop);
-        node.removeEventListener('mouseleave', handleStop);
-        node.removeEventListener('keydown', handleKeyboardStart);
-        node.removeEventListener('keyup', handleKeyboardStop);
-      },
-    };
   }
-  return null;
+
+  function destroy() {
+    node.classList.remove('s-ripple-container');
+    node.removeEventListener('touchstart', handleStart);
+    node.removeEventListener('touchmove', handleRemove);
+    node.removeEventListener('touchcancel', handleRemove);
+    node.removeEventListener('touchend', handleStop);
+    node.removeEventListener('mousedown', handleStart);
+    node.removeEventListener('mouseup', handleStop);
+    node.removeEventListener('mouseleave', handleStop);
+    node.removeEventListener('keydown', handleKeyboardStart);
+    node.removeEventListener('keyup', handleKeyboardStop);
+  }
+
+  if (options) setup();
+
+  return {
+    update(newOptions) {
+      options = newOptions;
+      if (options) setup();
+      else destroy();
+    },
+    destroy,
+  };
 };
