c6ebf3c1ab73512ef1c595ad50bea30954811e5e
TheComputerM <mudit.harshit99@outlook.com>
2020-09-19 15:29:18 +0530

    docs: add chips playground

---
+++ packages/docs/playground/chips.svelte
@@ -0,0 +1,43 @@
+<script>
+  import Playground from '@shared/Playground.svelte';
+  import { Chip, Avatar, Icon } from 'svelte-materialify/src';
+
+  const variants = ['label', 'link', 'outlined', 'pill'];
+
+  const controls = {
+    close: { type: 'switch' },
+    active: { type: 'switch' },
+    avatar: { type: 'switch' },
+    icon: { type: 'text' },
+    size: {
+      type: 'select',
+      items: ['x-small', 'small', 'default', 'large', 'x-large'],
+    },
+  };
+
+  const formatVariant = (v) => Object.fromEntries(v.map((i) => [i, true]));
+
+  let values = {
+    variants: [],
+    close: false,
+    active: true,
+    avatar: false,
+    icon: 'account-circle',
+    size: ['default'],
+  };
+</script>
+
+<Playground {controls} {variants} bind:values>
+  <Chip
+    size={values.size[0]}
+    close={values.close}
+    bind:active={values.active}
+    {...formatVariant(values.variants)}>
+    {#if values.avatar}
+      <Avatar class="primary-color">
+        <Icon class="mdi mdi-{values.icon}" />
+      </Avatar>
+    {/if}
+    <span> Chip Component </span>
+  </Chip>
+</Playground>
