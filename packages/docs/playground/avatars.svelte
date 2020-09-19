4a8f293222031f8a654a219ece91ee9f330118aa
TheComputerM <mudit.harshit99@outlook.com>
2020-09-19 22:44:12 +0530

    docs: added avatars for playground

---
+++ packages/docs/playground/avatars.svelte
@@ -0,0 +1,30 @@
+<script>
+  import Playground from '@shared/Playground.svelte';
+  import { Avatar, Icon } from 'svelte-materialify/src';
+
+  const controls = {
+    size: { type: 'slider' },
+    tile: { type: 'switch' },
+    icon: { type: 'switch' },
+    color: {
+      type: 'select',
+      items: ['primary', 'secondary', 'success', 'error', 'info'],
+    },
+  };
+
+  let values = {
+    variants: [],
+    size: 48,
+    tile: false,
+    icon: false,
+    color: ['primary'],
+  };
+</script>
+
+<Playground {controls} bind:values>
+  <Avatar tile={values.tile} size="{values.size}px" class="{values.color[0]}-color">
+    {#if values.icon}
+      <Icon class="mdi mdi-home" />
+    {:else}MS{/if}
+  </Avatar>
+</Playground>
