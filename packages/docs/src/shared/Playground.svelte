91164b4d9d581160f97b6930c4bf08d03bec44a6
TheComputerM <mudit.harshit99@outlook.com>
2020-09-19 09:37:38 +0530

    docs: added playground support for mobile

--- packages/docs/src/shared/Playground.svelte
+++ packages/docs/src/shared/Playground.svelte
@@ -35,10 +35,12 @@
     display: flex;
     background-color: var(--theme-app-bar);
     align-items: center;
+    height: 44px;
   }
 
   .playground__content {
-    height: 100%;
+    min-height: 100px;
+    height: calc(100% - 44px);
     width: 100%;
     display: flex;
     align-items: center;
@@ -47,52 +49,57 @@
 </style>
 
 <div class="playground rounded">
-  <div class="playground__toolbar">
-    {#if variants}
-      <SlideGroup bind:value={values.variants} multiple>
-        {#each variants as variant}
-          <SlideItem value={variant} let:active>
-            <button
-              class="s-btn depressed tile text size-large"
-              class:primary-text={active}
-              class:active
-              use:Ripple>
-              <span class="s-btn__content"> {variant} </span>
-            </button>
-          </SlideItem>
-        {/each}
-      </SlideGroup>
-    {/if}
-    <div style="margin-left:auto">
-      <Button icon on:click={toggleTheme}>
-        <Icon class="mdi mdi-invert mdi-invert-colors" />
-      </Button>
-    </div>
-  </div>
   <Row noGutters>
-    <Col {cols}>
+    <Col md={cols} cols="12">
+      <div class="playground__toolbar">
+        {#if variants}
+          <SlideGroup bind:value={values.variants} multiple>
+            {#each variants as variant}
+              <SlideItem value={variant} let:active>
+                <button
+                  class="s-btn depressed tile text size-large"
+                  class:primary-text={active}
+                  class:active
+                  use:Ripple>
+                  <span class="s-btn__content"> {variant} </span>
+                </button>
+              </SlideItem>
+            {/each}
+          </SlideGroup>
+        {/if}
+      </div>
       <div class="playground__content theme--{theme}">
         <slot />
       </div>
     </Col>
-    <Col cols={12 - cols} class="pa-2">
-      <h6 class="mb-3">Options</h6>
-      {#each Object.keys(controls) as label}
-        {#if controls[label].type === 'switch'}
-          <Switch bind:checked={values[label]}>{label}</Switch>
-        {:else if controls[label].type === 'slider'}
-          <Slider bind:value={values[label]}>{label}</Slider>
-        {:else if controls[label].type === 'select'}
-          <Select bind:value={values[label]} items={controls[label].items}>
-            {label}
-            <div slot="item" let:item>
-              <ListItem value={item}>{item}</ListItem>
-            </div>
-          </Select>
-        {:else if controls[label].type === 'text'}
-          <TextField bind:value={values[label]}>{label}</TextField>
-        {/if}
-      {/each}
+    <Col md={12 - cols} cols="12">
+      <div class="playground__toolbar justify-space-between pl-1 pr-1">
+        <div class="text-h6">Options</div>
+        <Button icon on:click={toggleTheme}>
+          <Icon class="mdi mdi-invert mdi-invert-colors" />
+        </Button>
+      </div>
+      <div class="pa-1">
+        {#each Object.keys(controls) as label}
+          <div class="mb-2" />
+          {#if controls[label].type === 'switch'}
+            <Switch bind:checked={values[label]}>
+              <span class="text--secondary">{label}</span>
+            </Switch>
+          {:else if controls[label].type === 'slider'}
+            <Slider bind:value={values[label]}>{label}</Slider>
+          {:else if controls[label].type === 'select'}
+            <Select bind:value={values[label]} items={controls[label].items}>
+              {label}
+              <div slot="item" let:item>
+                <ListItem value={item}>{item}</ListItem>
+              </div>
+            </Select>
+          {:else if controls[label].type === 'text'}
+            <TextField bind:value={values[label]}>{label}</TextField>
+          {/if}
+        {/each}
+      </div>
     </Col>
   </Row>
 </div>
