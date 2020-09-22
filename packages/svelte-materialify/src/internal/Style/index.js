c394e6699c2e5a7f237803d1301e0bac70bbf9cf
TheComputerM <mudit.harshit99@outlook.com>
2020-09-22 20:44:06 +0530

    fix: add px to numeric inputs

--- packages/svelte-materialify/src/internal/Style/index.js
+++ packages/svelte-materialify/src/internal/Style/index.js
@@ -1,3 +1,8 @@
+function format(input) {
+  if (typeof input === 'number') return `${input}px`;
+  return input;
+}
+
 /**
  * @param node {Element}
  * @param styles {Object}
@@ -5,14 +10,14 @@
 export default (node, _styles) => {
   let styles = _styles;
   Object.entries(styles).forEach(([key, value]) => {
-    if (value) node.style.setProperty(`--s-${key}`, value);
+    if (value) node.style.setProperty(`--s-${key}`, format(value));
   });
 
   return {
     update(newStyles) {
       Object.entries(newStyles).forEach(([key, value]) => {
         if (value) {
-          node.style.setProperty(`--s-${key}`, value);
+          node.style.setProperty(`--s-${key}`, format(value));
           delete styles[key];
         }
       });
