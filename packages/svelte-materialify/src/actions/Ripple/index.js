5305b3d3d8b5ba6ae6ee724f3dd6f1f851c03521
TheComputerM <mudit.harshit99@outlook.com>
2020-09-22 13:27:37 +0530

    fix: updated event listeners

--- packages/svelte-materialify/src/actions/Ripple/index.js
+++ packages/svelte-materialify/src/actions/Ripple/index.js
@@ -12,7 +12,6 @@ export default (node, _options = {}) => {
     ripple = RippleStart(e, options);
   };
   const handleStop = () => RippleStop(ripple);
-  const handleRemove = () => ripple.remove();
   const handleKeyboardStart = (e) => {
     if (!keyboardActive && (e.keyCode === 13 || e.keyCode === 32)) {
       ripple = RippleStart(e, { ...options, centered: true });
@@ -26,13 +25,9 @@ export default (node, _options = {}) => {
 
   function setup() {
     node.classList.add('s-ripple-container');
-    node.addEventListener('touchstart', handleStart, { passive: true });
-    node.addEventListener('touchmove', handleRemove, { passive: true });
-    node.addEventListener('touchcancel', handleRemove, false);
-    node.addEventListener('touchend', handleStop, { passive: true });
-    node.addEventListener('mousedown', handleStart);
-    node.addEventListener('mouseup', handleStop);
-    node.addEventListener('mouseleave', handleStop);
+    node.addEventListener('pointerdown', handleStart);
+    node.addEventListener('pointerup', handleStop);
+    node.addEventListener('pointerleave', handleStop);
     node.addEventListener('keydown', handleKeyboardStart);
     node.addEventListener('keyup', handleKeyboardStop);
     destroyed = false;
@@ -40,13 +35,9 @@ export default (node, _options = {}) => {
 
   function destroy() {
     node.classList.remove('s-ripple-container');
-    node.removeEventListener('touchstart', handleStart);
-    node.removeEventListener('touchmove', handleRemove);
-    node.removeEventListener('touchcancel', handleRemove);
-    node.removeEventListener('touchend', handleStop);
-    node.removeEventListener('mousedown', handleStart);
-    node.removeEventListener('mouseup', handleStop);
-    node.removeEventListener('mouseleave', handleStop);
+    node.removeEventListener('pointerdown', handleStart);
+    node.removeEventListener('pointerup', handleStop);
+    node.removeEventListener('pointerleave', handleStop);
     node.removeEventListener('keydown', handleKeyboardStart);
     node.removeEventListener('keyup', handleKeyboardStop);
     destroyed = true;
