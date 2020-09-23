821b868fe3bb53c5007650c980772212b28e526d
TheComputerM <mudit.harshit99@outlook.com>
2020-09-23 13:31:24 +0530

    chore: adding types

---
+++ packages/svelte-materialify/@types/Ripple.d.ts
@@ -0,0 +1,21 @@
+interface RippleOptions {
+  color?: string;
+  class?: string;
+  opacity?: number;
+  centered?: boolean;
+  spreadingDuration?: string;
+  spreadingDelay?: string;
+  spreadingTimingFunction?: string;
+  clearingDuration?: string;
+  clearingDelay?: string;
+  clearingTimingFunction?: string;
+}
+
+interface RippleReturn {
+  update(): void;
+  destroy(): void;
+}
+
+declare function Ripple(node: HTMLElement, options: RippleOptions): RippleReturn;
+
+export default Ripple;
