08ed3e4e337f79df338485b3f0ce246d6c5aafd8
TheComputerM <mudit.harshit99@outlook.com>
2020-10-15 14:28:07 +0530

    docs: added preview command

--- packages/docs/scripts/examples.js
+++ packages/docs/scripts/examples.js
@@ -4,7 +4,7 @@ require('prism-svelte');
 module.exports = {
   name: 'examples',
   transform(code, id) {
-    if (id.includes('\\examples\\')) {
+    if (id.includes('examples')) {
       const highlighted = Prism.highlight(code, Prism.languages.svelte, 'svelte');
       const output = `<script context="module">export const source = ${JSON.stringify(
         highlighted,
