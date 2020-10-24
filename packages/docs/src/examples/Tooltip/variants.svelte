f21d73d1137dca00bc68c42c0a7b2ce79ae21e14
TheComputerM <mudit.harshit99@outlook.com>
2020-10-24 17:55:53 +0530

    docs: update example

--- packages/docs/src/examples/Tooltip/variants.svelte
+++ packages/docs/src/examples/Tooltip/variants.svelte
@@ -1,24 +1,20 @@
 <script>
-  import { Row, Col, Button, Tooltip, Icon } from 'svelte-materialify/src';
+  import { Button, Tooltip, Icon } from 'svelte-materialify/src';
 </script>
 
-<Row class="text-center align-center justify-space-around">
-  <Col>
-    <Tooltip bottom>
-      <Button>Button</Button>
-      <span slot="tip">Tooltip</span>
-    </Tooltip>
-  </Col>
-  <Col>
-    <Tooltip bottom>
-      <Icon class="mdi mdi-home" />
-      <span slot="tip">Tooltip</span>
-    </Tooltip>
-  </Col>
-  <Col>
-    <Tooltip bottom>
-      <span>This text has a tooltip</span>
-      <span slot="tip">Tooltip</span>
-    </Tooltip>
-  </Col>
-</Row>
+<div class="d-flex justify-space-around align-center">
+  <Tooltip bottom>
+    <Button>Button</Button>
+    <span slot="tip">Tooltip</span>
+  </Tooltip>
+
+  <Tooltip bottom>
+    <Icon class="mdi mdi-home" />
+    <span slot="tip">Tooltip</span>
+  </Tooltip>
+
+  <Tooltip bottom>
+    <span>This text has a tooltip</span>
+    <span slot="tip">Tooltip</span>
+  </Tooltip>
+</div>
