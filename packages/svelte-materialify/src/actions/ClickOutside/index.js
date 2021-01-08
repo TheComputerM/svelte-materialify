c1165924c3d8cd0631e9fbd1599e57bc90a3e35b
bielinskim <45104530+bielinskim@users.noreply.github.com>
2021-01-08 12:28:11 +0100

    fix(clickOutside): set event propagation to capturing phase
    Co-authored-by: bielinskim <bielinskiim@gmail.com>

--- packages/svelte-materialify/src/actions/ClickOutside/index.js
+++ packages/svelte-materialify/src/actions/ClickOutside/index.js
@@ -10,7 +10,7 @@ export default (node, _options = {}) => {
       node.dispatchEvent(new CustomEvent('clickOutside'));
     }
   }
-  document.addEventListener('click', detect, { passive: true });
+  document.addEventListener('click', detect, { passive: true, capture: true });
   return {
     destroy() {
       document.removeEventListener('click', detect);
