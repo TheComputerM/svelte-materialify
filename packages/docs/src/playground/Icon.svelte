94f925f6770780cc3dbb442494d35a6080964d8f
Florian-Schoenherr <florian.schoenherr99@gmail.com>
2021-03-21 10:10:10 +0100

    docs: update Chip and Icon playgrounds
    Signed-off-by: Florian-Schoenherr <florian.schoenherr99@gmail.com>

--- packages/docs/src/playground/Icon.svelte
+++ packages/docs/src/playground/Icon.svelte
@@ -15,7 +15,7 @@
     icon: {
       type: 'select',
       items,
-      format: (val) => items.find((i) => i.value == val).name, // eslint-disable-line eqeqeq
+      format: (val) => items.find((i) => i.value == val) ? items.find((i) => i.value == val).name : '', // eslint-disable-line eqeqeq
     },
     color: { type: 'select', items: ['red', 'green', 'blue', 'indigo', 'purple'] },
   };
