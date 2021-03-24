ca2824eb847828118f9f1cb12a9a3e21cdb5183b
Greg Ristow <gristow@utheory.com>
2021-03-23 23:53:17 -0400

    style(docs): Improve NumberField validation example layout

--- packages/docs/src/examples/NumberField/validation.svelte
+++ packages/docs/src/examples/NumberField/validation.svelte
@@ -4,11 +4,11 @@
 
 <Row>
   <Col>
-    <NumberField min={3} value={3}>min=3</NumberField>
-    <NumberField max={3} value={3}>max=3</NumberField>
+    <NumberField class="mb-6" min={3} value={3}>min=3</NumberField>
+    <NumberField class="mb-6" max={3} value={3}>max=3</NumberField>
   </Col>
   <Col>
-    <NumberField step={10}>step=10</NumberField>
-    <NumberField step={10} value={3}>step=1.5 min=3 max=6</NumberField>
+    <NumberField class="mb-6" step={10}>step=10</NumberField>
+    <NumberField class="mb-6" step={10} value={3}>step=1.5 min=3 max=6</NumberField>
   </Col>
 </Row>
