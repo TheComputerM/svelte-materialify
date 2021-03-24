11b8f7faac6251a84a0edeeb973d3076a0c18233
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2021-03-24 16:19:29 -0400

    Move noUiSlider library import into onMount
    Based on these two resources :
    
    - https://github.com/TheComputerM/svelte-materialify/issues/171#issuecomment-802991037
    - https://kit.svelte.dev/docs#troubleshooting-server-side-rendering

--- packages/svelte-materialify/src/components/Slider/Slider.svelte
+++ packages/svelte-materialify/src/components/Slider/Slider.svelte
@@ -1,7 +1,3 @@
-<script context="module">
-  import noUiSlider from './nouislider.min';
-</script>
-
 <script>
   import Input from '../Input';
   import { onMount, afterUpdate, createEventDispatcher } from 'svelte';
@@ -51,7 +47,9 @@
     return thumb;
   }
 
-  onMount(() => {
+  onMount(async () => {
+    const { default: noUiSlider } = await import('./nouislider.min');
+    
     noUiSlider.cssClasses.tooltip = `tooltip ${thumbClass}`;
     noUiSlider.create(sliderElement, {
       cssPrefix: 's-slider__',
