839670de55831c5763cc3d8f763920c1ef353ea1
Marek Dědič <developer@dedic.eu>
2021-04-07 13:40:59 +0200

    fix(DataTable): the DataTable component now uses th properly

--- packages/svelte-materialify/src/components/DataTable/DataTableHead.svelte
+++ packages/svelte-materialify/src/components/DataTable/DataTableHead.svelte
@@ -1,6 +1,10 @@
 <script>
+  import { setContext } from 'svelte';
+
   let klass = '';
   export { klass as class };
+
+  setContext('is-table-head', true);
 </script>
 
 <style lang="scss" src="./DataTableHead.scss" global>
